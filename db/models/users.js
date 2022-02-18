3832e67c6d1abb38b0c48e0dbcd72d27cb65a72c
VforVogt <ChrisVGT@outlook.com>
2022-02-17 19:28:46 -0600

    test

--- db/models/users.js
+++ db/models/users.js
@@ -1,43 +1,43 @@
 // grab our db client connection to use with our adapters
-const client = require('../client');
+const client = require("../client");
 
-const bcrypt = require('bcrypt')
-
+const bcrypt = require("bcrypt");
 
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
-
-};
-
-
   createUser,
 };
 
-
-async function createUser({ username, password, firstName, lastName, email, phoneNumber }) {
+async function createUser({
+  username,
+  password,
+  firstName,
+  lastName,
+  email,
+  phoneNumber,
+}) {
   try {
     const saltRounds = 50;
-    const hashedPassword = await bcrypt.hash(password, saltRounds)
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     const {
       rows: [user],
-    } = await client.query(`
+    } = await client.query(
+      `
       INSERT INTO users (username, password, firstName, lastName, email, phoneNumber)
       VALUES ($1, $2, $3, $4, $5, $6)
       ON CONFLICT DO NOTHING
       RETURNING *;
-    `, [username, hashedPassword, firstName, lastName, email, phoneNumber])
+    `,
+      [username, hashedPassword, firstName, lastName, email, phoneNumber]
+    );
 
-
-    delete user.password
+    delete user.password;
     return user;
-
   } catch (error) {
-    throw error
+    throw error;
   }
 }
-
-
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */

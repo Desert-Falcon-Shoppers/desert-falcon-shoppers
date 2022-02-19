8b880baab44e7277a84fb7ef12615835fea9909c
shapirodaniel <shapirodanieladam@gmail.com>
2022-02-19 16:24:10 -0600

    add: ci log

--- db/client.js
+++ db/client.js
@@ -7,6 +7,10 @@ const DB_NAME = process.env.CI ? 'test-db' : 'desertfalconsdb';
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 
+if (process.env.CI) {
+  console.log('db url is: ', process.env.DATABASE_URL);
+}
+
 const client = new Client(DB_URL);
 
 module.exports = client;

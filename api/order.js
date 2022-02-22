540612e8c2468a68eef9abe94ea5911525b80b39
devinvogt2 <vogtdevin@ymail.com>
2022-02-21 21:05:26 -0600

    fix

--- api/order.js
+++ api/order.js
@@ -2,15 +2,6 @@ const express = require("express");
 const orderRouter = express.Router();
 const { OrderItems } = require("../db");
 const { createOrderDetails } = require("../db/models/order_details");
-<<<<<<< HEAD
-const authorizeUser = require("./auth");
-module.exports = orderRouter;
-
-orderRouter.post("/", authorizeUser, async (req, res, next) => {
-  try {
-    const { productId, orderId, quantity, price } = req.body;
-    const order = await Order.createOrderItems({
-=======
 const { deleteProduct } = require("../db/models/product");
 // const authorizeUser = require("./auth");
 authorizeUser = require("./auth");
@@ -20,7 +11,6 @@ orderRouter.post("/", async (req, res, next) => {
   try {
     const { productId, orderId, quantity, price } = req.body;
     const order = await OrderItems.createOrderItems({
->>>>>>> 2844493cecd0ee0b19c56d6f42ef5f84d6d75045
       productId,
       orderId,
       quantity,
@@ -32,11 +22,7 @@ orderRouter.post("/", async (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
-orderRouter.get("/:id", authorizeUser, async (req, res, next) => {
-=======
 orderRouter.get("/:id", async (req, res, next) => {
->>>>>>> 2844493cecd0ee0b19c56d6f42ef5f84d6d75045
   try {
     const orderDetails = await createOrderDetails();
     res.send(orderDetails);
@@ -45,11 +31,7 @@ orderRouter.get("/:id", async (req, res, next) => {
   }
 });
 
-<<<<<<< HEAD
-orderRouter.delete("/:orderId", authorizeUser, async (req, res, next) => {
-=======
 orderRouter.delete("/:productId", async (req, res, next) => {
->>>>>>> 2844493cecd0ee0b19c56d6f42ef5f84d6d75045
   try {
     const order = await OrderItems.deleteOrderItem(req.params.orderId);
     res.send(order);

ee36a1fdef7a2c58e346cb96f05ca2eaff2a407b
Dolton Scott <Doltonscott1@gmail.com>
2022-02-21 21:49:26 -0600

    db for discount

--- db/models/discount.js
+++ db/models/discount.js
@@ -1,38 +1,69 @@
 const client = require("../client");
 
 module.exports = {
-  createDiscounts,
-  getAllDiscounts,
+  createDiscount,
+  deleteDiscount,
+  updateDiscount,
 };
 
-async function createDiscounts({ name, description, discountAmount, active }) {
+async function createDiscount({ name, description, discountAmount, active }) {
   try {
     const {
-      rows: [discounts],
+      rows: [discount],
     } = await client.query(
       `
                 INSERT INTO discount (name, description, "discountAmount", active)
                 VALUES ($1, $2, $3, $4)
                 ON CONFLICT DO NOTHING
-                RETURNING *;
+                RETURNING *; 
                 `,
       [name, description, discountAmount, active]
     );
-    return discounts;
+    return discount;
   } catch (error) {
     throw error;
   }
 }
 
-async function getAllDiscounts() {
+async function updateDiscount({
+  id,
+  name,
+  description,
+  discountAmount,
+  active,
+}) {
   try {
     const {
-      rows: discounts
-    } = await client.query(`
-      SELECT * FROM discount
-    `)
-    return discounts
+      rows: [discount],
+    } = await client.query(
+      `
+       UPDATE discount
+       SET id=$1, name=$2, description=$3, "discountAmount"=$4, active=$5}
+       WHERE id=$6
+       RETURNING *;
+       `,
+      [id, name, description, discountAmount, active]
+    );
+    return discount;
   } catch (error) {
-    throw error
+    throw error;
+  }
+}
+
+async function deleteDiscount(discountId) {
+  try {
+    const {
+      rows: [discount],
+    } = await client.query(
+      `
+        DELETE FROM discount
+        WHERE id=$1
+        RETURNING *;
+      `,
+      [discountId]
+    );
+    return discount;
+  } catch (err) {
+    throw err;
   }
 }

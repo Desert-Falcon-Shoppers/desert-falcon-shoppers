fa5aab546b4c28834ad14c6aba1fa4e85bde135d
devinvogt2 <vogtdevin@ymail.com>
2022-02-22 23:39:01 -0600

    add: added COMPLETE CRUD for order.js

--- api/order.js
+++ api/order.js
@@ -1,11 +1,55 @@
 const express = require("express");
 const orderRouter = express.Router();
+const { OrderDetails } = require("../db");
 const { OrderItems } = require("../db");
-const { createOrderDetails } = require("../db/models/order_details");
-const { deleteProduct } = require("../db/models/product");
 // const authorizeUser = require("./auth");
 authorizeUser = require("./auth");
 module.exports = orderRouter;
+
+orderRouter.get("/", async (req, res, next) => {
+  try {
+    const orderDetails = await OrderDetails.getAllOrderDetails();
+    res.send(orderDetails);
+  } catch (error) {
+    next(error);
+  }
+});
+
+orderRouter.get("/details", async (req, res, next) => {
+  try {
+    const orderDetails = await OrderDetails.getAllOrderDetails();
+    res.send(orderDetails);
+  } catch (error) {
+    next(error);
+  }
+});
+
+orderRouter.get("/items", async (req, res, next) => {
+  try {
+    const orderItems = await OrderItems.getAllOrderItems();
+    res.send(orderItems);
+  } catch (error) {
+    next(error);
+  }
+});
+
+orderRouter.get("/items/:id", async (req, res, next) => {
+  try {
+    const orderItems = await OrderItems.getOrderItemsById(req.params.id);
+    res.send(orderItems);
+  } catch (error) {
+    next(error);
+  }
+});
+
+orderRouter.get("/details/:id", async (req, res, next) => {
+  try {
+    const orderDetails = await OrderDetails.getOrderDetailsById(req.params.id);
+    res.send(orderDetails);
+  } catch (error) {
+    next(error);
+  }
+});
 
 orderRouter.post("/", async (req, res, next) => {
   try {
@@ -22,18 +66,60 @@ orderRouter.post("/", async (req, res, next) => {
   }
 });
 
-orderRouter.get("/:id", async (req, res, next) => {
+orderRouter.patch("/items/:id", async (req, res, next) => {
   try {
-    const orderDetails = await createOrderDetails();
-    res.send(orderDetails);
+    const { productId, orderId, quantity, price } = req.body;
+
+    const updateOrderItems = {
+      productId,
+      orderId,
+      quantity,
+      price,
+    };
+
+    const orderItems = await updateOrderItems(
+      req.params.orderItemsId,
+      updateOrderItems
+    );
+    res.status(303).send({ orderItems });
   } catch (error) {
     next(error);
   }
 });
 
-orderRouter.delete("/:productId", async (req, res, next) => {
+orderRouter.patch("/details/:id", async (req, res, next) => {
+  try {
+    const { productId, orderId, quantity, price } = req.body;
+
+    const updateOrderDetails = {
+      productId,
+      orderId,
+      quantity,
+      price,
+    };
+
+    const orderDetails = await updateOrderDetails(
+      req.params.orderDetailsId,
+      updateOrderDetails
+    );
+    res.status(303).send({ orderDetails });
+  } catch (error) {
+    next(error);
+  }
+});
+
+orderRouter.delete("/:orderId", async (req, res, next) => {
   try {
     const order = await OrderItems.deleteOrderItem(req.params.orderId);
+    res.send(order);
+  } catch (error) {
+    next(error);
+  }
+});
+
+orderRouter.delete("/:orderId", async (req, res, next) => {
+  try {
+    const order = await OrderDetails.deleteOrderDetails(req.params.orderId);
     res.send(order);
   } catch (error) {
     next(error);
--- db/models/order_items.js
+++ db/models/order_items.js
@@ -3,6 +3,9 @@ const client = require("../client");
 module.exports = {
   createOrderItems,
   deleteOrderItem,
+  getAllOrderItems,
+  updateOrderItems,
+  getOrderItemsById,
 };
 
 async function createOrderItems({ productId, orderId, quantity, price }) {
@@ -15,6 +18,56 @@ async function createOrderItems({ productId, orderId, quantity, price }) {
          VALUES ($1, $2, $3, $4)
          RETURNING *;
          `,
+      [productId, orderId, quantity, price]
+    );
+    return orderItems;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllOrderItems() {
+  try {
+    const { rows: orders } = await client.query(
+      `
+       SELECT * FROM order_items;
+       `
+    );
+
+    return orders;
+  } catch (error) {
+    throw err;
+  }
+}
+
+async function getOrderItemsById(orderItemsId) {
+  try {
+    const {
+      rows: [orderItems],
+    } = await client.query(
+      `
+        SELECT * FROM order_items
+        WHERE id=$1;
+        `,
+      [orderItemsId]
+    );
+    return orderItems;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateOrderItems({ productId, orderId, quantity, price }) {
+  try {
+    const {
+      rows: [orderItems],
+    } = await client.query(
+      `
+       UPDATE order_items
+       SET id=$1, productId=$2, orderId=$3, quantity=$4, price=$5}
+       WHERE id=$6
+       RETURNING *;
+       `,
       [productId, orderId, quantity, price]
     );
     return orderItems;

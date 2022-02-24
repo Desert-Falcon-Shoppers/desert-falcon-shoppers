bb37701f3ffcc1aff91b5979053e8b042564a8fc
Jacob Kelcy <jacobkelcy23@gmail.com>
2022-02-23 22:43:06 -0600

    prod-inv-func: added functions to product_inventory db and api

--- db/models/product_inventory.js
+++ db/models/product_inventory.js
@@ -1,7 +1,10 @@
 const client = require("../client");
+const { getProductById } = require("./product");
 
 module.exports = {
   createProductInventory,
+  getProductInventoryById,
+  updateProductInventory,
 };
 
 async function createProductInventory({ productQuantity }) {
@@ -10,9 +13,9 @@ async function createProductInventory({ productQuantity }) {
       rows: [productInventory],
     } = await client.query(
       `
-               INSERT INTO product_inv ("productQuantity")
-               VALUES ($1)
-               RETURNING *;
+    INSERT INTO product_inv ("productQuantity")
+    VALUES ($1)
+    RETURNING *;
                `,
       [productQuantity]
     );
@@ -20,4 +23,49 @@ async function createProductInventory({ productQuantity }) {
   } catch (error) {
     throw error;
   }
+} // delete and update
+
+async function getProductInventoryById(productInventoryId) {
+  try {
+    const {
+      rows: [productInventory],
+    } = await client.query(
+      `
+      SELECT * FROM product_inv
+      WHERE id=$1;
+      `,
+      [productInventoryId]
+    );
+    return productInventory;
+  } catch (error) {
+    throw error;
+  }
 }
+
+productRouter.patch("/inventory/:id", async (req, res, next) => {
+  try {
+    const { id, productQuantity } = req.body;
+
+    const updatedProductInventory = {
+      id,
+      productQuantity,
+    };
+
+    const productInventory = await updateProductInventory(
+      req.params.id,
+      updatedProductInventory
+    );
+    res.status(303).send({ productInventory });
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.delete("/inventory/:id", async (req, res, next) => {
+  try {
+    const productInventory = await deleteProductInventory(req.params.id);
+    res.delete(productInventory);
+  } catch (error) {
+    next(error);
+  }
+});

fd941b90b658c226f796dc48b3cd8c99b6c4e13c
Dolton Scott <Doltonscott1@gmail.com>
2022-02-23 20:41:36 -0600

    added update and patch for shop

--- api/shop.js
+++ api/shop.js
@@ -1,30 +1,28 @@
-const express = require("express")
+const express = require("express");
 const shopRouter = express.Router();
 
-const { ShopSession } = require("../db")
+const { ShopSession } = require("../db");
 
-const { deleteShopSession } = require("../db/models/product")
-module.exports = shopRouter
-
+const { deleteShopSession } = require("../db/models/product");
+module.exports = shopRouter;
 
 shopRouter.get("/", async (req, res, next) => {
-    try {
-        const shopSession = await ShopSession.getAllShoppingSessions()
-        res.send(shopSession)
-    } catch (error) {
-        next(error)
-    }
-})
+  try {
+    const shopSession = await ShopSession.getAllShoppingSessions();
+    res.send(shopSession);
+  } catch (error) {
+    next(error);
+  }
+});
 
 shopRouter.post("/", async (req, res, next) => {
-    try {
-        const { userId, total } = req.body;
-        const shopSession = await ShopSession.createShopSession({ userId, total });
-        res.send({ shopSession });
-
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { userId, total } = req.body;
+    const shopSession = await ShopSession.createShopSession({ userId, total });
+    res.send({ shopSession });
+  } catch (error) {
+    next(error);
+  }
 });
 
 shopRouter.get("/:id", async (req, res, next) => {
@@ -36,13 +34,22 @@ shopRouter.get("/:id", async (req, res, next) => {
   }
 });
 
+shopRouter.patch("/:id", async (req, res, next) => {
+  try {
+    const { id, name, description } = req.body;
+    const updatedShop = { id, name, description };
+    const shop = await updatedShop(req.params.id, updatedShop);
+    res.status(303).send({ shop });
+  } catch (err) {
+    next(err);
+  }
+});
+
 shopRouter.delete("/:id", async (req, res, next) => {
-    try {
-        const shopSession = await deleteShopSession(req.params.id);
-        res.delete(shopSession);
-    } catch (error) {
-        throw error
-    }
-})
-
-
+  try {
+    const shopSession = await deleteShopSession(req.params.id);
+    res.delete(shopSession);
+  } catch (error) {
+    next(error);
+  }
+});

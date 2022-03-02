f7dad0b7b7f3dfa576f9048ad585209b52215928
Jacob Kelcy <jacobkelcy23@gmail.com>
2022-03-02 00:06:23 -0600

    footer: created footer

--- src/components/SignupOrLogin.js
+++ src/components/SignupOrLogin.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import ReactDom from 'react-dom';
-import './styles/SignupOrLogin.css';
+import React from "react";
+import ReactDom from "react-dom";
+import "./styles/SignupOrLogin.css";
 
 function SignupOrLogin() {
-    return (
-        <div className="sign_login_container">
-            <h1>Sign Up</h1>
-            <form>
-                <input type="text" name="username" placeholder='Username'></input>
-                <input type="password" name="password" placeholder='Password'></input>
-                <input type="text" name="firstName" placeholder='First Name'></input>
-                <input type="text" name="lastName" placeholder='Last Name'></input>
-                <input type="email" name="email" placeholder='Email'></input>
-                <input type="text" name="phoneNumber" placeholder='Mobile'></input>
-                <button type="submit" class="signUpBtn">Sign Up</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="sign_login_container">
+      <h1>Sign Up</h1>
+      <form>
+        <input type="text" name="username" placeholder="Username"></input>
+        <input type="password" name="password" placeholder="Password"></input>
+        <input type="text" name="firstName" placeholder="First Name"></input>
+        <input type="text" name="lastName" placeholder="Last Name"></input>
+        <input type="email" name="email" placeholder="Email"></input>
+        <input type="text" name="phoneNumber" placeholder="Mobile"></input>
+        <button type="submit" class="signUpBtn">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default SignupOrLogin
+export default SignupOrLogin;

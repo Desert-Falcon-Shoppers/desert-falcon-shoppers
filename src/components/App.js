3f3be172616064abef8307e335e919d0f726ce57
Joelfolske <joel_folske@icloud.com>
2022-03-02 21:26:27 -0600

    header and nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Checkout, SignupOrLogin, } from '.';
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Checkout, SignupOrLogin } from ".";
+import Header from "./Header";
+import Nav from "./Nav";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <SignupOrLogin path="/login" component={SignupOrLogin} />
+        <Header path="/header" component={Header} />
         {/* route props are things like a wildcard that exists at that particular route */}
         {/* <Route path="/checkout" component={Checkout} /> */}
       </Switch>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./styles/header.css";
 
 function Nav() {
-    return (
-        <div>Nav</div>
-    )
+  return (
+    <div>
+      <a className="1"></a>
+      <a className="2"></a>
+      <a className="3"></a>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;

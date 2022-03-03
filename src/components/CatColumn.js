b0d50aad849224c9eca126c07bbcbc883c5ca0b5
Sean Barker <sean.p.barker86@gmail.com>
2022-03-02 21:21:18 -0600

    started categories

---
+++ src/components/CatColumn.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles/CatColumn.css";
+
+function CatColumn() {
+  return (
+    <main>
+      <div className="categories">
+        <a href="#">Luxury Watches</a>
+        <a href="#">Sports Watches</a>
+        <a href="#">Smart Watches</a>
+        <a href="#">Pocket Watches</a>
+        <a href="#">Brand</a>
+        <a href="#">Product Material</a>
+        <a href="#">Color</a>
+        <a href="#">Size</a>
+      </div>
+    </main>
+  );
+}
+
+export default CatColumn;

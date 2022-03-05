2eab17780179fbcab003eaa9ff2e33c7e6aeebab
VforVogt <ChrisVGT@outlook.com>
2022-03-05 01:32:30 -0600

    New framework for the navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Checkout, Signup, Login, Home, ProductRows, CatColumn, Footer } from ".";
+import {
+  Checkout,
+  Signup,
+  Login,
+  Home,
+  ProductRows,
+  CatColumn,
+  Footer,
+  Nav,
+} from ".";
 
 const App = () => {
   return (
-    <div>
-      <div>
-        <Router>
-          <Switch>
-            <Signup path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
-            <Route path="/footer" component={Footer} />
-            <Route path="/categories" component={CatColumn} />
-            <Route path="/home" component={Home} />
-            <Route path="/productrows" component={ProductRows} />
-            {/* route props are things like a wildcard that exists at that particular route */}
-            {/* <Route path="/checkout" component={Checkout} /> */}
-          </Switch>
-        </Router>
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Signup path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/footer" component={Footer} />
+        <Route path="/categories" component={CatColumn} />
+        <Route path="/home" component={Home} />
+        <Route path="/productrows" component={ProductRows} />
+        <Route path="/nav" component={Nav} />
+        {/* route props are things like a wildcard that exists at that particular route */}
+        {/* <Route path="/checkout" component={Checkout} /> */}
+      </Switch>
+    </Router>
   );
 };
 

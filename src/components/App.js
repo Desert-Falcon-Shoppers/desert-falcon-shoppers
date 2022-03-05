7ab427a102a5dca5c71bba2e8899fba9c34dfc37
Jacob Kelcy <jacobkelcy23@gmail.com>
2022-03-04 21:52:45 -0600

    footer/header: footer is done just needs query. Header is half way done

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Checkout, SignupOrLogin, Home } from ".";
 import Footer from "./Footer";
+import Header from "./Header";
 
 const App = () => {
   return (
@@ -11,6 +12,10 @@ const App = () => {
           <Switch>
             <SignupOrLogin path="/Login" component={SignupOrLogin} />
             <Footer path="/Footer" component={Footer} />
+            <Header
+              path="/Header"
+              component={<Header path="/Header" component={Header} />}
+            />
             <Home path="/home" component={Home} />
             {/* route props are things like a wildcard that exists at that particular route */}
             {/* <Route path="/checkout" component={Checkout} /> */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./styles/Header.css";
 
 function Header() {
-    return (
-        <div>Header</div>
-    )
+  return (
+    <div className="headerContainer">
+      <div className="imgContainer">
+        <img
+          className="logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/07/Danvers_Falcons_logo_.png"
+        ></img>
+      </div>
+      <div className="theRest">
+        <a href="http://localhost:3000/Login" className="login">
+          Login/Register
+        </a>
+        <input
+          className="search"
+          type="text"
+          name="search-term"
+          placeholder="search"
+        ></input>
+        <img
+          className="cartIcon"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXbVFhiyjsifqC1o7WL0LdisB9I_Vz5BM12g&usqp=CAU"
+        ></img>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;

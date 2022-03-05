72066f30961fa6c4bbe2eef048cf0b5cc73a85f8
VforVogt <ChrisVGT@outlook.com>
2022-03-05 01:47:11 -0600

    new changes to navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
 import {
   Checkout,
   Signup,
@@ -9,26 +8,8 @@ import {
   ProductRows,
   CatColumn,
   Footer,
-  Nav,
+  Header,
 } from ".";
-
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Signup path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path="/footer" component={Footer} />
-        <Route path="/categories" component={CatColumn} />
-        <Route path="/home" component={Home} />
-        <Route path="/productrows" component={ProductRows} />
-        <Route path="/nav" component={Nav} />
-        {/* route props are things like a wildcard that exists at that particular route */}
-        {/* <Route path="/checkout" component={Checkout} /> */}
-      </Switch>
-    </Router>
-=======
-import { Checkout, Signup, Login, Home, ProductRows, CatColumn, Footer, Header } from ".";
 
 const App = () => {
   return (
@@ -55,7 +36,6 @@ const App = () => {
         </Router>
       </div>
     </div>
->>>>>>> 408c5a6d90b38579320f4adc3641777bee2e093c
   );
 };
 

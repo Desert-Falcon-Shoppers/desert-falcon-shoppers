4fc32d4f286d448c517f9452a740bb81cb757ea0
devinvogt2 <vogtdevin@ymail.com>
2022-03-08 23:07:04 -0600

    added our hardcoded version of my orders!

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,13 +7,13 @@ function Signup() {
         <div className="signup_container">
             <h1 className="signup_header">Sign Up</h1>
             <form className="signup_form">
-                <input type="text" name="username" placeholder="Username"></input>
-                <input type="password" name="password" placeholder="Password"></input>
-                <input type="text" name="firstName" placeholder="First Name"></input>
-                <input type="text" name="lastName" placeholder="Last Name"></input>
-                <input type="email" name="email" placeholder="Email"></input>
-                <input type="text" name="phoneNumber" placeholder="Mobile"></input>
-                <button type="submit" class="signUpBtn">
+                <input className="signup_form_input" type="text" name="username" placeholder="Username"></input>
+                <input className="signup_form_input" type="password" name="password" placeholder="Password"></input>
+                <input className="signup_form_input" type="text" name="firstName" placeholder="First Name"></input>
+                <input className="signup_form_input" type="text" name="lastName" placeholder="Last Name"></input>
+                <input className="signup_form_input" type="email" name="email" placeholder="Email"></input>
+                <input className="signup_form_input" type="text" name="phoneNumber" placeholder="Mobile"></input>
+                <button className="signup_form_input" type="submit" class="signUpBtn">
                     Sign Up
                 </button>
             </form>

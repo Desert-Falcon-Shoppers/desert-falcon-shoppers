141ce8974685e0f8522223a8a321e4ee899f4172
shapirodaniel <shapirodanieladam@gmail.com>
2022-03-12 10:43:26 -0600

    fix: login flow

--- src/custom-hooks/useLogin.js
+++ src/custom-hooks/useLogin.js
@@ -1,44 +1,43 @@
-import { useState, useContext } from "react"
-import { useHistory } from "react-router-dom";
-import { logInUser } from "../axios-services";
-import { AuthContext } from "../context/AuthContext";
+import { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { logInUser } from '../axios-services';
+import { AuthContext } from '../context/AuthContext';
 
 export function useLogin() {
-    const { updateAuthStatus } = useContext(AuthContext)
-    const history = useHistory()
+  const { updateAuthStatus } = useContext(AuthContext);
+  const history = useHistory();
 
-    const [form, setForm] = useState({
-        username: "",
-        password: "",
-    })
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+  });
 
-    const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.value]: e.target.value })
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      console.log(e);
+      e.preventDefault();
+      console.log({ form });
+
+      const { message, token } = await logInUser(form);
+      localStorage.setItem('capstone_token', token);
+      setError(null);
+      updateAuthStatus();
+      history.push('/productrows');
+    } catch (error) {
+      setError(error);
+      console.error(error);
     }
-
-    const handleSubmit = async (e) => {
-        try {
-            console.log(e)
-            e.preventDefault();
-            console.log({ form })
-
-            const { message, token } = await logInUser(form)
-            localStorage.setItem('capstone_token', token)
-            setError(null)
-            updateAuthStatus();
-            history.push('/productrows')
-        } catch (error) {
-            setError(error)
-            console.error(error)
-        }
-    }
-    return {
-        h1: "Login",
-        error,
-        handleSubmit,
-        handleChange,
-    }
+  };
+  return {
+    h1: 'Login',
+    error,
+    handleSubmit,
+    handleChange,
+  };
 }
-
